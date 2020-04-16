76bbcf142adb68308b43dc95e4c9891755ac56c3
eklemen <ejklemen@gmail.com>
2020-04-15 21:37:47 -0400

    removed whitespaces for formatting

--- templates/js/models/sequelize/index.js
+++ templates/js/models/sequelize/index.js
@@ -18,17 +18,17 @@ if (config.use_env_variable) {
 const db = {};
 
 fs.readdirSync(__dirname)
-  .filter(function (file) {
+  .filter(function(file) {
     return (
       file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
     );
   })
-  .forEach(function (file) {
+  .forEach(function(file) {
     var model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(function (modelName) {
+Object.keys(db).forEach(function(modelName) {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }

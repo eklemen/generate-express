71759e0410738dc95a4491297a240b3eeb22a472
Sujil Maharjan <maharjansujil@gmail.com>
2020-04-18 20:54:50 -0400

    Moved the static string text to different file

---
+++ js/code-snippets.js
@@ -0,0 +1,52 @@
+
+const codeSnippets = {
+    mongoJsCode: 
+`const dbUri = process.env.MONGODB_URI || 'mydb';
+const collections = ['mycollection'];
+
+const db = mongojs(dbUri, collections);`,
+
+    sequelizeCode: 
+`// Run sequelize before listen
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(port, function() {
+    console.log("App listening on PORT " + port);
+    });
+});`,
+
+    sequelizeEnvironmentVars: 
+`USERNAME=root
+PASSWORD=null
+DATABASE=database_dev
+HOST=127.0.0.1
+DB_PORT=3306
+DIALECT=mysql`,
+
+    mongoMongooseCode:
+`const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/mydb';
+const mongooseConfigs = { useNewUrlParser: true };
+mongoose.connect(mongoUri, mongooseConfigs);`,
+
+    redisCode:
+`/**
+* Redis Setup. For more options for redis client, go to: https://www.npmjs.com/package/redis#options-object-properties
+*/
+const redisPort = process.env.REDIS_PORT || 6379;
+const redisHost = process.env.REDIS_HOST || '127.0.0.1';
+const redisClient = redis.createClient(redisPort, redisHost);
+
+redisClient.on("error", (error) =>  {
+ console.error(error);
+});
+
+redisClient.on('connect', () => {
+ console.log(\`Redis connected in port: \${redisPort}\`)
+})
+// --------------End of Redis Setup-----------------------`,
+
+    redisEnvironmentVars:
+`REDIS_PORT=6379
+REDIS_HOST=127.0.0.1`
+}
+
+module.exports = codeSnippets;

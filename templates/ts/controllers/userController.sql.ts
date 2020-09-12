b465c769c93c4742b6fafa93a7b8395ab3f1d7a6
eklemen <ejklemen@gmail.com>
2020-09-12 13:46:15 -0400

    return data from sequelize query

--- templates/ts/controllers/userController.sql.ts
+++ templates/ts/controllers/userController.sql.ts
@@ -2,7 +2,7 @@ import {Request, Response, NextFunction} from 'express';
 import User from '../models/User';
 
 export const getAllUsers = (req: Request, res: Response, next: NextFunction) => {
-  User.findAll({}).then(() => {
-    res.send('User route');
+  User.findAll({}).then((data) => {
+    res.send({name: 'User Route', data});
   })
 };

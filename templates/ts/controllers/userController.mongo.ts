02e2d847122dbdd47130b5aacba0eb10a51673c1
eklemen <ejklemen@gmail.com>
2020-09-12 22:00:32 -0400

    returned data

--- templates/ts/controllers/userController.mongo.ts
+++ templates/ts/controllers/userController.mongo.ts
@@ -2,7 +2,7 @@ import {Request, Response, NextFunction} from 'express';
 import {User} from '../models';
 
 export const getAllUsers = (req: Request, res: Response, next: NextFunction) => {
-  User.find().then(() => {
-    res.send('User route');
+  User.find().then((data) => {
+    res.send({name: 'User Route', data});
   })
 };
